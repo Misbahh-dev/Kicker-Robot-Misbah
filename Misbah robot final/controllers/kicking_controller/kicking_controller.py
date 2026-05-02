a1db0d3342a5eb43f4820fb113598c45ee102d3e
Misbahh-dev <Misbah.rr42@gmail.com>
2026-05-02 23:15:59 +0500

    Commit 4 - Added Ball detection  function

--- Misbah robot final/controllers/kicking_controller/kicking_controller.py
+++ Misbah robot final/controllers/kicking_controller/kicking_controller.py
@@ -1,4 +1,44 @@
+"""
+Robot Kicker – added detect_ball() function
+"""
+import cv2
+import numpy as np
 from controller import Robot, Motion
+
+# Colour thresholds
+BALL_WHITE_LOWER = np.array([0,   0, 180], dtype=np.uint8)
+BALL_WHITE_UPPER = np.array([180, 50, 255], dtype=np.uint8)
+BALL_BLACK_LOWER = np.array([0,   0,   0], dtype=np.uint8)
+BALL_BLACK_UPPER = np.array([180, 80,  55], dtype=np.uint8)
+GRASS_LOWER      = np.array([35,  60,  60], dtype=np.uint8)
+GRASS_UPPER      = np.array([85, 255, 255], dtype=np.uint8)
+
+MIN_BALL_AREA = 400
+
+def detect_ball(image_data, width, height):
+    frame = np.frombuffer(image_data, dtype=np.uint8).reshape((height, width, 4))
+    bgr   = frame[:, :, :3].copy()
+    hsv   = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+
+    not_grass  = cv2.bitwise_not(cv2.inRange(hsv, GRASS_LOWER, GRASS_UPPER))
+    white_mask = cv2.bitwise_and(cv2.inRange(hsv, BALL_WHITE_LOWER, BALL_WHITE_UPPER), not_grass)
+    black_mask = cv2.bitwise_and(cv2.inRange(hsv, BALL_BLACK_LOWER, BALL_BLACK_UPPER), not_grass)
+
+    k_expand  = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+    dilated_w = cv2.dilate(white_mask, k_expand, iterations=2)
+    ball_mask = cv2.bitwise_or(white_mask, cv2.bitwise_and(dilated_w, black_mask))
+
+    ball_mask = cv2.morphologyEx(ball_mask, cv2.MORPH_CLOSE,
+                                  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15)))
+    ball_mask = cv2.morphologyEx(ball_mask, cv2.MORPH_OPEN,
+                                  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+
+    contours, _ = cv2.findContours(ball_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        if cv2.contourArea(c) >= MIN_BALL_AREA:
+            # ball detected
+            return True
+    return False
 
 class NaoGoalTracker(Robot):
     S_SEARCH_BALL = 'SEARCH_BALL'
@@ -6,7 +46,6 @@ class NaoGoalTracker(Robot):
     def __init__(self):
         super().__init__()
         self.timeStep = int(self.getBasicTimeStep())
-
         self.cameraTop = self.getDevice('CameraTop')
         self.cameraBottom = self.getDevice('CameraBottom')
         self.cameraTop.enable(4 * self.timeStep)
@@ -19,9 +58,9 @@ class NaoGoalTracker(Robot):
 
         self.currently_playing = None
         self.cooldown = 0
-
         self.state = self.S_SEARCH_BALL
-        print('[NaoGoalTracker] State: SEARCH_BALL')
+        self.cam_w_bot = self.cameraBottom.getWidth()
+        self.cam_h_bot = self.cameraBottom.getHeight()
 
     def _play_motion(self, motion, cooldown=67):
         if self.currently_playing and not self.currently_playing.isOver():
@@ -33,16 +72,22 @@ class NaoGoalTracker(Robot):
     def _is_busy(self):
         return self.cooldown > 0 or (self.currently_playing and not self.currently_playing.isOver())
 
-    def _state_search_ball(self):
-        print('[NaoGoalTracker] Waiting for ball ...')
+    def _state_search_ball(self, ball_detected):
+        if ball_detected:
+            print('[NaoGoalTracker] Ball found!')
+        else:
+            print('[NaoGoalTracker] Searching ...')
 
     def run(self):
         while self.step(self.timeStep) != -1:
             if self.cooldown > 0:
                 self.cooldown -= 1
 
+            bot_raw = self.cameraBottom.getImage()
+            ball_detected = detect_ball(bot_raw, self.cam_w_bot, self.cam_h_bot)
+
             if self.state == self.S_SEARCH_BALL:
-                self._state_search_ball()
+                self._state_search_ball(ball_detected)
 
 robot = NaoGoalTracker()
 robot.run()
