f99c15ec87bb9d17089b2971d5215f9e3b48fa16
Misbahh-dev <Misbah.rr42@gmail.com>
2026-05-03 15:08:43 +0500

    Commit 5 - Transition to Align Ball

--- Misbah robot final/controllers/kicking_controller/kicking_controller.py
+++ Misbah robot final/controllers/kicking_controller/kicking_controller.py
@@ -1,47 +1,42 @@
 """
-Robot Kicker – added detect_ball() function
+Robot Kicker – switch to ALIGN_BALL state on detection
 """
 import cv2
 import numpy as np
 from controller import Robot, Motion
 
-# Colour thresholds
+# Colour thresholds (same as commit 4)
 BALL_WHITE_LOWER = np.array([0,   0, 180], dtype=np.uint8)
 BALL_WHITE_UPPER = np.array([180, 50, 255], dtype=np.uint8)
 BALL_BLACK_LOWER = np.array([0,   0,   0], dtype=np.uint8)
 BALL_BLACK_UPPER = np.array([180, 80,  55], dtype=np.uint8)
 GRASS_LOWER      = np.array([35,  60,  60], dtype=np.uint8)
 GRASS_UPPER      = np.array([85, 255, 255], dtype=np.uint8)
-
 MIN_BALL_AREA = 400
 
 def detect_ball(image_data, width, height):
     frame = np.frombuffer(image_data, dtype=np.uint8).reshape((height, width, 4))
     bgr   = frame[:, :, :3].copy()
     hsv   = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
-
     not_grass  = cv2.bitwise_not(cv2.inRange(hsv, GRASS_LOWER, GRASS_UPPER))
     white_mask = cv2.bitwise_and(cv2.inRange(hsv, BALL_WHITE_LOWER, BALL_WHITE_UPPER), not_grass)
     black_mask = cv2.bitwise_and(cv2.inRange(hsv, BALL_BLACK_LOWER, BALL_BLACK_UPPER), not_grass)
-
     k_expand  = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
     dilated_w = cv2.dilate(white_mask, k_expand, iterations=2)
     ball_mask = cv2.bitwise_or(white_mask, cv2.bitwise_and(dilated_w, black_mask))
-
     ball_mask = cv2.morphologyEx(ball_mask, cv2.MORPH_CLOSE,
                                   cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15)))
     ball_mask = cv2.morphologyEx(ball_mask, cv2.MORPH_OPEN,
                                   cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
-
     contours, _ = cv2.findContours(ball_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
         if cv2.contourArea(c) >= MIN_BALL_AREA:
-            # ball detected
             return True
     return False
 
 class NaoGoalTracker(Robot):
-    S_SEARCH_BALL = 'SEARCH_BALL'
+    S_SEARCH_BALL   = 'SEARCH_BALL'
+    S_ALIGN_BALL    = 'ALIGN_BALL'
 
     def __init__(self):
         super().__init__()
@@ -74,9 +69,13 @@ class NaoGoalTracker(Robot):
 
     def _state_search_ball(self, ball_detected):
         if ball_detected:
-            print('[NaoGoalTracker] Ball found!')
+            print('[NaoGoalTracker] Ball found! -> ALIGN_BALL')
+            self.state = self.S_ALIGN_BALL
         else:
             print('[NaoGoalTracker] Searching ...')
+
+    def _state_align_ball(self):
+        print('[NaoGoalTracker] Aligning (placeholder)')
 
     def run(self):
         while self.step(self.timeStep) != -1:
@@ -88,6 +87,8 @@ class NaoGoalTracker(Robot):
 
             if self.state == self.S_SEARCH_BALL:
                 self._state_search_ball(ball_detected)
+            elif self.state == self.S_ALIGN_BALL:
+                self._state_align_ball()
 
 robot = NaoGoalTracker()
 robot.run()
