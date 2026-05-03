1f878008b243990c5d77ac6877b523664ed3dd75
Misbahh-dev <Misbah.rr42@gmail.com>
2026-05-03 16:26:35 +0500

    Commit 6 - Ball Alignment w  sidesteooing

--- Misbah robot final/controllers/kicking_controller/kicking_controller.py
+++ Misbah robot final/controllers/kicking_controller/kicking_controller.py
@@ -1,11 +1,11 @@
 """
-Robot Kicker – switch to ALIGN_BALL state on detection
+Robot Kicker – ALIGN_BALL uses ball centre to sidestep
 """
 import cv2
 import numpy as np
 from controller import Robot, Motion
 
-# Colour thresholds (same as commit 4)
+# Colour thresholds and constants
 BALL_WHITE_LOWER = np.array([0,   0, 180], dtype=np.uint8)
 BALL_WHITE_UPPER = np.array([180, 50, 255], dtype=np.uint8)
 BALL_BLACK_LOWER = np.array([0,   0,   0], dtype=np.uint8)
@@ -13,15 +13,18 @@ BALL_BLACK_UPPER = np.array([180, 80,  55], dtype=np.uint8)
 GRASS_LOWER      = np.array([35,  60,  60], dtype=np.uint8)
 GRASS_UPPER      = np.array([85, 255, 255], dtype=np.uint8)
 MIN_BALL_AREA = 400
+BALL_CENTRE_MIN = 0.48
+BALL_CENTRE_MAX = 0.502
+BALL_CENTRE_MID = (BALL_CENTRE_MIN + BALL_CENTRE_MAX) / 2.0
 
 def detect_ball(image_data, width, height):
     frame = np.frombuffer(image_data, dtype=np.uint8).reshape((height, width, 4))
     bgr   = frame[:, :, :3].copy()
     hsv   = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
-    not_grass  = cv2.bitwise_not(cv2.inRange(hsv, GRASS_LOWER, GRASS_UPPER))
+    not_grass = cv2.bitwise_not(cv2.inRange(hsv, GRASS_LOWER, GRASS_UPPER))
     white_mask = cv2.bitwise_and(cv2.inRange(hsv, BALL_WHITE_LOWER, BALL_WHITE_UPPER), not_grass)
     black_mask = cv2.bitwise_and(cv2.inRange(hsv, BALL_BLACK_LOWER, BALL_BLACK_UPPER), not_grass)
-    k_expand  = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+    k_expand = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
     dilated_w = cv2.dilate(white_mask, k_expand, iterations=2)
     ball_mask = cv2.bitwise_or(white_mask, cv2.bitwise_and(dilated_w, black_mask))
     ball_mask = cv2.morphologyEx(ball_mask, cv2.MORPH_CLOSE,
@@ -30,9 +33,12 @@ def detect_ball(image_data, width, height):
                                   cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
     contours, _ = cv2.findContours(ball_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
-        if cv2.contourArea(c) >= MIN_BALL_AREA:
-            return True
-    return False
+        area = cv2.contourArea(c)
+        if area >= MIN_BALL_AREA:
+            x, y, w, h = cv2.boundingRect(c)
+            cx_norm = (x + w/2.0) / width
+            return True, cx_norm, area
+    return False, 0.5, 0.0
 
 class NaoGoalTracker(Robot):
     S_SEARCH_BALL   = 'SEARCH_BALL'
@@ -56,6 +62,7 @@ class NaoGoalTracker(Robot):
         self.state = self.S_SEARCH_BALL
         self.cam_w_bot = self.cameraBottom.getWidth()
         self.cam_h_bot = self.cameraBottom.getHeight()
+        self.last_ball_cx = 0.5
 
     def _play_motion(self, motion, cooldown=67):
         if self.currently_playing and not self.currently_playing.isOver():
@@ -69,13 +76,28 @@ class NaoGoalTracker(Robot):
 
     def _state_search_ball(self, ball_detected):
         if ball_detected:
-            print('[NaoGoalTracker] Ball found! -> ALIGN_BALL')
+            print('[NaoGoalTracker] Ball found -> ALIGN_BALL')
             self.state = self.S_ALIGN_BALL
         else:
             print('[NaoGoalTracker] Searching ...')
 
-    def _state_align_ball(self):
-        print('[NaoGoalTracker] Aligning (placeholder)')
+    def _state_align_ball(self, ball_detected, ball_cx):
+        if not ball_detected:
+            print('[NaoGoalTracker] Ball lost during align')
+            return
+        if BALL_CENTRE_MIN <= ball_cx <= BALL_CENTRE_MAX:
+            print(f'[NaoGoalTracker] Ball centred (cx={ball_cx:.3f})')
+            # Next commit will add APPROACH
+            return
+        if self._is_busy():
+            return
+        error = ball_cx - BALL_CENTRE_MID
+        if error > 0:
+            print(f'[NaoGoalTracker] Ball RIGHT (cx={ball_cx:.3f}) -> sidestep right')
+            self._play_motion(self.mot_side_right)
+        else:
+            print(f'[NaoGoalTracker] Ball LEFT  (cx={ball_cx:.3f}) -> sidestep left')
+            self._play_motion(self.mot_side_left)
 
     def run(self):
         while self.step(self.timeStep) != -1:
@@ -83,12 +105,15 @@ class NaoGoalTracker(Robot):
                 self.cooldown -= 1
 
             bot_raw = self.cameraBottom.getImage()
-            ball_detected = detect_ball(bot_raw, self.cam_w_bot, self.cam_h_bot)
+            ball_detected, ball_cx, _ = detect_ball(bot_raw, self.cam_w_bot, self.cam_h_bot)
+            if ball_detected:
+                self.last_ball_cx = ball_cx
+            ball_cx = self.last_ball_cx
 
             if self.state == self.S_SEARCH_BALL:
                 self._state_search_ball(ball_detected)
             elif self.state == self.S_ALIGN_BALL:
-                self._state_align_ball()
+                self._state_align_ball(ball_detected, ball_cx)
 
 robot = NaoGoalTracker()
 robot.run()
